23c45595a2219a6a6bcfb53043ae95a890358888
CT Arrington <ct@nowhere.com>
2012-11-04 21:50:51 -0500

    Added functions to replace lines and write over file

--- GlobalFindAndReplace/FindAndReplace.pl
+++ GlobalFindAndReplace/FindAndReplace.pl
@@ -17,15 +17,47 @@ sub get_lines {
    return @lines;
 }
 
+sub write_lines {
+   my($file, @lines) = @_;
+
+   open (FH, "> $file") or die "Can't open $file for write: $!";
+   
+   foreach (@lines) {
+      print FH;
+   }
+
+   close FH or die "Cannot close $file: $!";
+
+   return @lines;
+}
+
+sub replace_lines {
+   my(@lines) = @_;
+   my(@replacements);
+   
+   foreach (@lines) {
+       if(/^%/) {  
+          print "cl = $_  \n";
+          s/foo/bar/g;
+          push(@replacements, $_);
+       }
+       else {
+           push(@replacements, $_);
+       }
+   }
+
+   return @replacements;
+}
+
 sub wanted {
    return unless -f;
    return unless $_ =~ /.*txt$/; 
 
    my(@lines) = &get_lines($_);
+   my(@replacements) = &replace_lines(@lines);
+   &write_lines($_, @replacements);
 
-
-   print $File::Find::name, "\n";
-   print @lines;
+   print @replacements;
    print "\n\n";
    
 }
